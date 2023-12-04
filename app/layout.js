48c23522c7f51a5fb6f2d48b4406f2ec3be2900f
pevescarol <carol.peves@gmail.com>
2023-12-03 21:23:09 -0300

    config generales

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,19 @@
-import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Navbar from '../components/navbar/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Sthep Peves Dev',
-  description: 'Portfolio Sthepany Peves',
+  title: 'Sthep Peves | Portfolio Dev',
+  description: 'Hi I&apos;m Sthepany Peves. Tech person who builds web products.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className='grainy'>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
