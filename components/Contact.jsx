329b8d8c94f7284f47529ceb185d47f60f9f287f
pevescarol <carol.peves@gmail.com>
2024-01-02 14:02:13 -0300

    nueva imagen

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,19 +2,25 @@ import Link from 'next/link'
 import {FiLinkedin, FiMail} from 'react-icons/fi'
 import {BiLogoTelegram} from 'react-icons/bi'
 import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
+import Image from 'next/image'
+import carIlus from '../public/assets/car-ilus.svg'
 
 const Contact = () => {
   return (
-    <section id='contact' className='max-w-screen-lg mx-auto py-16 px-6 lg:px-24'>
-      <div className='flex flex-col items-center text-center py-20 rounded-xl bg-hero-b bg-center bg-cover text-white'>
-        <h3 className="text-[32px] tracking-wide text-subtext  font-semibold">
+    <section id='contact' className='max-w-7xl mx-auto my-24 px-8 '>
+      
+
+      <div className='flex items-center justify-center flex-col md:flex-row text-center py-20 rounded-xl bg-hero-b bg-center bg-cover text-white'>
+        <div className="">
+          <Image src={carIlus} alt="" width={200} height={200}/>
+        </div>
+        <div>
+        <h3 className="text-3xl md:text-5xl tracking-wide text-subtext  font-semibold">
           Get in Touch.
-            {/* Let’s build <br className='hidden md:block' />
-            a project together */}
         </h3>
-        <p className='text-text text-[12px] pt-2'>So that we can talk more about . . .</p>
+        {/* <p className='text-text text-sm md:text-xl pt-2'>So that we can talk more about . . .</p> */}
         
-        <div className='flex items-center justify-center gap-4 text-xl mt-8 opacity-70'>
+        <div className='flex items-center justify-center gap-4 text-2xl mt-8 opacity-70'>
           <Link href="mailto:scpeves@gmail.com" className='group'>
             <FiMail className='group-hover:scale-125 transition' />
           </Link>
@@ -28,6 +34,7 @@ const Contact = () => {
             <AiFillGithub className='group-hover:scale-125 transition' />
           </Link>
           
+        </div>
         </div>
         
       </div>
