7dd1a0b596af5c6463b3a22a5ec1d74df64c57c1
pevescarol <carol.peves@gmail.com>
2024-01-02 14:19:09 -0300

    se modifica el responsive y nuevos proyectos

--- components/projects/CardProject.jsx
+++ components/projects/CardProject.jsx
@@ -5,9 +5,10 @@ import {HiCode, HiOutlineEye} from 'react-icons/hi'
 
 const CardProject = ({title, description, image, tags, demo, github}) => {
   return (
-    <div className='group hover:-translate-y-4 transition-all'>
-      <div className='relative h-36 md:h-40 rounded-t-xl'>
-        <Image src={image} alt='card image' className='object-cover h-36 md:h-40 rounded-t-xl' />
+    <figure className='group mb-4 flex flex-col gap-3 cursor-pointer'>
+
+      <div className='relative h-36 md:h-52 rounded-xl'>
+        <Image src={image} alt='card image' className='object-cover h-full rounded-xl' />
         <div className='rounded-t-xl overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 '>
           <Link href={github} className='h-14 w-14 mr-2 flex items-center border-2 relative rounded-full border-[#ADB7BE] hover:border-secondary group/link'>
             <HiCode className='h-10 w-10 text-text m-2 cursor-pointer group-hover/link:text-secondary z-1' />
@@ -19,20 +20,18 @@ const CardProject = ({title, description, image, tags, demo, github}) => {
         </div>
       </div>
       
-      {/* text */}
-      <div className="py-6 px-4 rounded-b-xl  bg-[#16141f6b]">
-          <h3 className='font-semibold text-xl text-heading'>{title}</h3>
-          <p className="text-sm text-text">
+          {/* text */}
+          <h3 className='text-lg md:text-xl font-bold group-hover:text-secondary'>{title}</h3>
+          <p className="text-text">
             {description}
           </p>
-          <ul className='flex gap-2 mt-2 flex-wrap' aria-label='Technologies used'>
+          <ul className='flex gap-2 flex-wrap' aria-label='Technologies used'>
             {tags.map((tag, index) => (
-              <li key={index} className='text-xs rounded-full bg-[#8079fd]/10 text-[#8079fd] px-3 py-1 font-medium leading-5'>{tag}</li>
+              <li key={index} className='text-xs opacity-70 text-[#8079fd] rounded-full bg-[#8079fd]/10 px-3 py-1 font-medium leading-5'>{tag}</li>
             ))}
           </ul>
-      </div>
       
-    </div>
+    </figure>
   )
 }
 
--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -4,37 +4,36 @@ import CardProject from './CardProject'
 
 import {FiArrowRight} from 'react-icons/fi'
 
-import deruta from '../../public/assets/deruta.jpg'
-import imgT from '../../public/assets/design.jpg'
+import coherence from '../../public/assets/coherencia-bg.jpg'
 import argencoin from '../../public/assets/img-argencoin.jpg'
+import tiendita from '../../public/assets/tiendita.jpg'
 
 
 const projects = [
   {
     title: 'ArgenCoin App',
-    tags: ['React', 'Next', 'Tailwind'],
-    description: 'View live quotes for cryptocurrencies, in addition to the dollar.',
+    tags: ['react', 'next', 'tailwind', 'api'],
+    description: 'View live quotes for the most important cryptocurrencies, in addition to the dollar.',
     demo: '',
     github: 'https://github.com/pevescarol/cotizaciones-dolar-crypto',
     img: argencoin,
   },
   {
-    title: 'DeRuta',
-    tags: ['React', 'Tailwind'],
-    description: 'Practice project of a responsive landing page.',
-    demo: 'https://deruta.vercel.app/',
-    github: 'https://github.com/pevescarol/DeRuta-web-frontend',
-    img: deruta,
-  },
-  {
-    title: 'Strava app',
-    tags: ['.NET', 'Javascript'],
-    description: 'Aplicacion de entrenamiento',
+    title: 'Ecommerce Store',
+    tags: ['react', 'next', 'tailwind'],
+    description: 'Virtual store of simple purchase by Whatsapp, with google sheets of database.',
     demo: 'https://www.google.com',
     github: 'https://www.github.com/pevescarol',
-    img: imgT,
+    img: tiendita,
   },
-  
+  {
+    title: 'Meme Generator',
+    tags: ['react', 'next', 'tailwind'],
+    description: 'App to create memes with image upload and export.',
+    demo: '',
+    github: 'https://github.com/pevescarol',
+    img: coherence,
+  },
 ]
 
 
@@ -52,39 +51,39 @@ const Projects = () => {
 
 
   return (
-    <section id='projects' className='max-w-screen-lg mx-auto flex flex-col items-center justify-center px-6 lg:px-24 my-24'>
-        <div className='w-full my-2 text-center mb-16'>
-          <h2 className='text-[28px] font-bold text-heading tracking-widest mb-4'>Some projects<span className='text-[#8c55ff] text-[25px]'>.</span></h2>
-          <p className="text-subtext md:text-lg leading-7 opacity-70 tracking-tight">I always build projects to learn, experiment, and grow.
+    <section id='projects' className='max-w-7xl mx-auto my-36 md:my-0 px-8 min-h-screen flex items-center justify-center'>
+      <div>
+        <div className='text-center mb-[76px]'>
+          <h2 className='text-2xl md:text-3xl lg:text-4xl tracking-wide font-bold text-heading'>some projects<span className='text-[#8c55ff] text-4xl'>.</span></h2>
+          <p className="text-base md:text-lg mt-6 leading-8 opacity-70">I always build projects to learn,<br className='block md:hidden' /> experiment, and grow.
           </p>
         </div>
-      
 
-      <div className='grid md:grid-cols-3 gap-8 md:gap-4 my-6 items-center '>
-          {/* {projects.map((item, index) => ( */}
-          {projects.slice(0, itemsToShow).map((item, index) => (
-            <CardProject 
-              key={index}
-              title={item.title} 
-              description={item.description}
-              image={item.img}
-              tags={item.tags}
-              github={item.github}
-              demo={item.demo}
-            />
-          ))}
-      </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 md:gap-6'>
+            {projects.slice(0, itemsToShow).map((item, index) => (
+              <CardProject 
+                key={index}
+                title={item.title} 
+                description={item.description}
+                image={item.img}
+                tags={item.tags}
+                github={item.github}
+                demo={item.demo}
+              />
+            ))}
+        </div>
 
-      
-      <div className='mt-12'>
-        {itemsToShow < 3 ? 
-          <button onClick={showmore}  className='text-md inline-flex items-center leading-tight font-semibold group' >
-            <span className='border-b border-transparent pb-px transition group-hover:border-secondary motion-reduce:transition-none'>
-              Load more projects
-            </span> 
-            <FiArrowRight className='ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none' />
-          </button>
-        : ''}
+        
+        <div className='mt-12'>
+          {itemsToShow < 3 ? 
+            <button onClick={showmore}  className='text-md inline-flex items-center leading-tight font-semibold group' >
+              <span className='border-b border-transparent pb-px transition group-hover:border-secondary motion-reduce:transition-none'>
+                Load more projects
+              </span> 
+              <FiArrowRight className='ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none' />
+            </button>
+          : ''}
+        </div>
       </div>
 
     </section>
