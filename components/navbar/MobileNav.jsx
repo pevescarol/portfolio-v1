b866cd740e895ad120c05a12c4445f1ed35fcc06
pevescarol <carol.peves@gmail.com>
2024-01-03 14:50:16 -0300

    modificicaiones en navbar

--- components/navbar/MobileNav.jsx
+++ components/navbar/MobileNav.jsx
@@ -6,16 +6,16 @@ const MobileNav = ({open, setOpen}) => {
       className={`absolute top-0 left-0 bottom-0 right-0 w-full h-screen text-center flex items-center justify-center bg-body transform ${open ? '-translate-y-0' : '-translate-y-full'} transition-transform duration-300 ease-in-out `}
     >
       <ul>
-        <li className="p-4 font-bold text-2xl md:text-4xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300  ">
+        <li className="p-4 font-bold text-2xl md:text-4xl lg:text-6xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300  ">
           <Link href='/'  onClick={() => setOpen(!open)}>Home</Link>
         </li>
-        <li className="p-4 font-bold text-2xl md:text-4xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300">
+        <li className="p-4 font-bold text-2xl md:text-4xl lg:text-6xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300">
           <Link href='#about'  onClick={() => setOpen(!open)}>About</Link>
         </li>
-        <li className="p-4 font-bold text-2xl md:text-4xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300">
+        <li className="p-4 font-bold text-2xl md:text-4xl lg:text-6xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300">
           <Link href='#projects' onClick={() => setOpen(!open)}>Projects</Link>
         </li>
-        <li className="p-4 font-bold text-2xl md:text-4xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300 ">
+        <li className="p-4 font-bold text-2xl md:text-4xl lg:text-6xl text-subtext hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-[#a8c0ff] via-[#8f5cff] to-[#7303c0] hover:duration-300 ">
           <Link href='#contact' onClick={() => setOpen(!open)}>Contact</Link>
         </li>
       </ul>
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -23,9 +23,10 @@ const Navbar = () => {
   return (
     <nav style={{backdropFilter: `${color}`}} className='w-full fixed top-0 left-0 z-40 ease duration-300 before:content-[""] before:absolute before:top-0 before:left-0 before:w-full before:h-[4px] before:bg-gradient-to-r from-body via-[#8f5cff] to-[#4e2df9] before:z-10'>
         <MobileNav open={navbar} setOpen={setNavbar} />
-        <div className='flex flex-wrap justify-between w-full p-8 pb-0 items-center max-w-6xl mx-auto'>
+
+        <div className='flex flex-wrap justify-between w-full px-8 py-6  items-center max-w-7xl mx-auto'>
           {/* logo */}
-          <h1 className='font-bold text-xl md:text-2xl'>SP<span className='text-[#8c55ff] text-[25px]'>.</span><span className='text-secondary'>_</span></h1>
+          <h1 className='font-bold text-xl md:text-2xl flex items-center'><span className='text-secondary font-normal'>&lt;</span>SCP<span className='text-secondary font-normal'>&frasl;&gt;</span></h1>
           {/* menu mobile */}
           <div className='mt-2 z-10 w-6 h-5 flex items-center text-3xl cursor-pointer group '>
             <button 
